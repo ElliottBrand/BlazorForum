b33253c8dcca566300c5b263bfc40cd4ebd57910
Steve Elliott <steve@elliottbrand.com>
2020-10-21 12:25:18 -0400

    Added testing related nuget packages and added a few tests.

---
+++ BlazorForum.Tests/Domain/Helpers/Forum/ForumTopicCountHelpersTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Moq;
+using BlazorForum.Domain.Helpers.Forum;
+using BlazorForum.Domain.Interfaces;
+
+namespace BlazorForum.Tests.Domain.Helpers.Forum
+{
+    // NOTE: Models namespace was left in to help prevent confusion between the classes and the components, since some are named the same
+    public class ForumTopicCountHelpersTests
+    {
+        Mock<IManageForumPosts> mockForumPostsManager = new Mock<IManageForumPosts>();
+
+        [Fact]
+        async void GetTopicsPostCountListAsync_ReturnsTopicPostCountListTest()
+        {
+            var fakeForumTopics = FakeData.FakeForumData.BuildForumTopicsList(2);
+            
+            mockForumPostsManager.Setup(p => p.GetApprovedForumPostsAsync(It.IsAny<int>()))
+                .Returns(Task.FromResult(new List<Models.ForumPost>()));
+
+            var result = await new ForumTopicCountHelpers(mockForumPostsManager.Object).GetTopicsPostCountListAsync(fakeForumTopics);
+
+            Assert.IsType<List<Models.TopicPostCount>>(result);
+            Assert.True(result.Count == 2);
+        }
+
+        [Fact]
+        void GetTopicCount_ReturnsZeroTopicCountWhenEmptyTest()
+        {
+            var fakeForumTopics = FakeData.FakeForumData.BuildForumTopicsList(2);
+
+            var result = ForumTopicCountHelpers.GetTopicCount(new List<Models.TopicPostCount>(), 1);
+
+            Assert.Equal(0, result);
+        }
+
+        [Fact]
+        void GetTopicCount_ReturnsZeroTopicCountWhenNullTest()
+        {
+            var result = ForumTopicCountHelpers.GetTopicCount(null, 1);
+
+            Assert.Equal(0, result);
+        }
+
+        [Fact]
+        void GetTopicCount_ReturnsTopicCountTest()
+        {
+            var fakeForumTopics = FakeData.FakeForumData.BuildForumTopicsList(2);
+
+            var result = ForumTopicCountHelpers.GetTopicCount(FakeData.FakeForumData.BuildTopicPostCountList(3, 2), 1);
+
+            Assert.IsType<int>(result);
+            Assert.Equal(2, result);
+        }
+    }
+}
---
+++ BlazorForum.Tests/FakeData/FakeForumData.cs
@@ -0,0 +1,53 @@
+﻿using BlazorForum.Tests.Domain.Helpers.Forum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorForum.Tests.FakeData
+{
+    public static class FakeForumData
+    {
+        public static List<Models.ForumTopic> BuildForumTopicsList(int listCount)
+        {
+            var list = new List<Models.ForumTopic>();
+
+            for (int i = 0; i < listCount; i++)
+            {
+                list.Add(BuildFakeForumTopic(i+1));
+            }
+
+            return list;
+        }
+
+        public static Models.ForumTopic BuildFakeForumTopic(int forumTopicId)
+        {
+            return new Models.ForumTopic()
+            {
+                ForumTopicId = forumTopicId
+            };
+        }
+
+        public static List<Models.TopicPostCount> BuildTopicPostCountList(int listCount, int childPostsCount)
+        {
+            var list = new List<Models.TopicPostCount>();
+
+            for (int i = 0; i < listCount; i++)
+            {
+                list.Add(BuildFakeTopicPostCount(i + 1, childPostsCount));
+            }
+
+            return list;
+        }
+
+        public static Models.TopicPostCount BuildFakeTopicPostCount(int parentItemId, int childCount)
+        {
+            return new Models.TopicPostCount()
+            {
+                ParentItemId = parentItemId,
+                ChildCount = childCount
+            };
+        }
+    }
+}
